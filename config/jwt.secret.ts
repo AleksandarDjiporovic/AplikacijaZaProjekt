34e85ed8b5812e52c8e592ceec4574669cd7a1de
Sale <lord.djipa@gmail.com>
2020-08-29 16:10:27 +0200

    Implement the use of JWT sale

---
+++ config/jwt.secret.ts
@@ -0,0 +1,5 @@
+export const jwtSecret: string = 'sd77sd7sdasdasdas0das00asd0as9d9asd' +
+                                 'afasf7a7sf7a7fa7sf7a7sf7assda7f7asf' +
+                                 'asdasdasas8a8sd88asd8asa8s8a8sa8as8' +
+                                 'agasg5a5s55asd5a5sd5a5sd55asd5a5sd5' +
+                                 'dasd7as7d7asd7as7d7as7d7asd7asa7s7d' ;
