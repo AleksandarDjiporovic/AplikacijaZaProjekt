f683cb404a500bdd1f21295b2935d07b08047479
Sale <lord.djipa@gmail.com>
2020-09-01 12:46:56 +0200

    Modify the cart presenataion logic

--- src/services/cart/cart.service.ts
+++ src/services/cart/cart.service.ts
@@ -13,13 +13,7 @@ export class CartService {
         private readonly cart: Repository<Cart>,
 
         @InjectRepository(CartArticle) 
-        private readonly cartArticle: Repository<CartArticle>,
-
-        @InjectRepository(Article) 
-        private readonly article: Repository<Article>,
-
-        @InjectRepository(Order) 
-        private readonly order: Repository<Order>,
+        private readonly cartArticle: Repository<CartArticle>,   
     ) { }
 
     async getLastActiveCartByUserid(userId: number): Promise<Cart | null> {
@@ -81,6 +75,7 @@ export class CartService {
                 "cartArticles",
                 "cartArticles.article",
                 "cartArticles.article.category",
+                "cartArticles.article.articlePrices",
             ],
         });
     }
