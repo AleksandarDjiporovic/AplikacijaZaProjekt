4e583a7f6e06456f52e70ebec2b5df319dce17ff
Sale <lord.djipa@gmail.com>
2020-09-02 13:59:53 +0200

    Implement order management for the administarator

---
+++ src/dtos/order/change.order.status.dto.ts
@@ -0,0 +1,9 @@
+import * as Validator from 'class-validator';
+
+export class ChangeOrderStatusDto {
+    @Validator.IsNotEmpty()
+    @Validator.IsString()
+    @Validator.IsIn(["rejected", "accepted", "shipped", "pending"])
+    newStatus: "rejected" | "accepted" | "shipped" | "pending";
+
+}
